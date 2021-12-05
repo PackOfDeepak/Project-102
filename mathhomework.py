7634c4807be5421e9b89420a9033e8e9d3fa69f4
PackOfDeepak <68823003+PackOfDeepak@users.noreply.github.com>
2021-12-05 07:51:46 -0600

    Add files via upload

---
+++ mathhomework.py
@@ -0,0 +1,17 @@
+import math
+#This is a program to automate the logarithm funciton
+#code for user to enter the base
+base = input("log to the base _____:")
+#code to convert the string of base to a number
+int(base)
+print(base)
+#code for user to enter the number of 
+x = input("log to the base B of _____")
+#code to convert strong of x to a number
+int(x)
+print(x)
+#code to do the logarithm
+ans = math.log(x[base])
+#code to print answer
+print("the answer to your log is...")
+print(ans)
